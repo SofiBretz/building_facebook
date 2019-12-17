001dcef072e87b027d3487cc28f5ff20283b79d7
Leo <ronald.mauritsson@gmail.com>
2019-12-17 18:52:45 +0300

    Customised the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    root 'devise/registrations#new'
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+    get 'logout', to: 'devise/sessions#destroy'
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
