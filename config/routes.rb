23e4ec1c0f2e5d4602bda1d38cbc94f0d2285ca1
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2019-12-19 17:03:37 -0300

    root rout changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_scope :user do
-    root 'devise/registrations#new'
+
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
     get 'logout', to: 'devise/sessions#destroy'
   end
   devise_for :users
+  root 'users#index'
   resources :posts, only: [:index, :show, :new, :create]
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
