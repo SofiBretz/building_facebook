cac3c15f0dbc7b6f7fb7005aaddc9345fc56db4f
Leo <ronald.mauritsson@gmail.com>
2020-01-02 19:39:07 +0300

    Removed non functional button in views, carried out successful rspec tests on the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,98 +1,35 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
+
 RSpec.describe User, type: :model do
-  context 'associations for 1 user that created 2 posts' do
-    before :example do
-      @user = User.create!(first_name: 'so',
-                           last_name: 'so',
-                           email: 'so@gmail.com',
-                           password: '123456',
-                           password_confirmation: '123456')
-      ​
-      @post1 = Post.create!(content: 'This is the content of post 1',
-                            user_id: @user.id)
-      ​
-      @post2 = Post.create!(content: 'This is the content of post 2',
-                            user_id: @user.id)
-    end
-    ​
-    it 'the user can access the created posts' do
-      expect(@user.posts).to eq([@post2, @post1])
-      expect(@user.posts.count).to eq(2)
-    end
-    ​
-    it 'the user can create a new post' do
-      post3 = @user.posts.create!(content: 'This is the content of post 3')
-      expect(@user.posts).to eq([post3, @post2, @post1])
-    end
+  before :each do
+    @user = User.create(first_name: 'test', last_name: 'test', email: 'test@test.com', password: '123456')
   end
-  ​
-  context 'validations of a new user' do
-    before :example do
-      @user = User.new(first_name: 'so', last_name: 'so', email: 'so@gmail.com', password: '123456',
-                       password_confirmation: '123456')
+
+  it 'is valid with valid attributes' do
+    expect(@user).to be_valid
+  end
+
+  context 'Name validation' do
+    it 'Valid name check' do
+      expect(@user).to be_valid
     end
-    ​
-    it 'checks that the name must be present' do
+
+    it 'Invalid name check ' do
       @user.first_name = ''
-      expect(@user.valid?).to eq(false)
-    end
-    ​
-    it 'checks that the name must be < 50 characters' do
-      @user.first_name = 'a' * 51
-      expect(@user.valid?).to eq(false)
-    end
-    ​
-    it 'checks that the name must be present' do
-      @user.last_name = ''
-      expect(@user.valid?).to eq(false)
-    end
-    ​
-    it 'checks that the name must be < 50 characters' do
-      @user.last_name = 'a' * 51
-      expect(@user.valid?).to eq(false)
-    end
-
-    it 'checks that email is in the correct format' do
-      invalid_emails = %w[myemail.com email@gmail examplegmail.com myemail @gmail.com]
-      invalid_emails.each do |email|
-        @user.email = email
-        expect(@user.valid?).to eq(false)
-      end
-    end
-    ​
-    it 'checks that email must be present and unique' do
-      @user.save
-      user2 = @user.dup
-      expect(user2.valid?).to eq(false)
+      expect(@user).to_not be_valid
     end
   end
 
-  context 'associations for 1 user that created 2 comments' do
-    before :example do
-      @user = User.create!(first_name: 'so',
-                           last_name: 'so',
-                           email: 'so@gmail.com',
-                           password: '123456',
-                           password_confirmation: '123456')
-      ​
-      @post = Post.create!(content: 'This is the content of post 1',
-                           user_id: @user.id)
-      @comment1 = Comment.create!(content: 'This is the first comment',
-                                  user_id: @user.id, post_id: @post.id)
-      @comment2 = Comment.create!(content: 'This is the second comment',
-                                  user_id: @user.id, post_id: @post.id)
+  context 'Email validation' do
+    it 'Valid email check' do
+      expect(@user).to be_valid
     end
-    ​
-    it 'the user can access the created comments' do
-      expect(@user.comments).to eq([@comment2, @comment1])
-      expect(@user.comments.count).to eq(2)
-    end
-    ​
-    it 'the user can create a new comment' do
-      comment3 = @user.comments.create!(content: 'This is the third comment', post_id: @post.id)
-      expect(@user.comments).to eq([comment3, @comment2, @comment1])
+
+    it 'Invalid email check' do
+      @user.email = 'xxx'
+      expect(@user).to_not be_valid
     end
   end
-  ​
 end
