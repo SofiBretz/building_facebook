b64bbafc13a976e32657ff5a68197f5ef22935fc
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2020-01-07 19:39:20 -0300

    added visit to the scenario on the test on new_post, added included config to the spec rails_helper

--- spec/feature/new_post_spec.rb
+++ spec/feature/new_post_spec.rb
@@ -9,12 +9,14 @@ RSpec.feature 'User Creates New Post', type: :feature do
   end
 
   scenario 'Valid Post' do
+    visit '/posts/new'
     fill_in('post[content]', with: 'Hello World')
     click_button('Create Post')
-    expect(page).to redirect_to(root_path)
+    expect(page).to have_current_path(root_path)
   end
 
   scenario 'Invalid Post(content length)' do
+    visit '/posts/new'
     click_button('Create Post')
     expect(page).to have_current_path(new_post_path)
   end
