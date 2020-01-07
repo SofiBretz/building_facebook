0517b7c7d4bbd104274d549293ee64e7fecf5f07
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2020-01-07 12:52:15 -0300

    added likes and comments controller, added resources to comments on the routes and added the methods

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   root 'posts#index'
   resources :posts, only: [:index, :show, :new, :create]
   resources :users, only: [:index, :show]
+  resources :comments, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
