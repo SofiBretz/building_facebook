159c4ca177a6407190c967b4db126ec1099642c5
Leo <ronald.mauritsson@gmail.com>
2020-01-08 20:20:05 +0300

    fixed rubocop errors

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,19 +1,17 @@
+# frozen_string_literal: true
+
 module FriendshipsHelper
   def friend_status(user)
     return unless current_user != user
 
     if current_user.friend? user
-      "Friends"
+      'Friends'
+    elsif current_user.pending_friends.include? user
+      'Waiting for Comfirmation'
+    elsif current_user.friend_requests.include? user
+      link_to 'Accept Friend Request', friendship_path(user), method: :patch
     else
-      if current_user.pending_friends.include? user
-        "Waiting for Comfirmation"
-      else
-        if current_user.friend_requests.include? user
-          link_to "Accept Friend Request", friendship_path(user), method: :patch
-        else
-          link_to "Send Friend Request", friendships_path(user), method: :post
-        end
-      end
+      link_to 'Send Friend Request', friendships_path(user), method: :post
     end
   end
 end
