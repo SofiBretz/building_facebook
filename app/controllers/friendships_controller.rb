92fe661d3b10ae2f33196dd3ec258ab219108143
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2020-01-09 15:14:46 -0300

    render error when send request fixed

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,14 +3,14 @@
 class FriendshipsController < ApplicationController
   def create
     user = User.find(params[:user_id])
-    return current_user.send_friend_request(user) if user
+    current_user.send_friend_request(user) if user
 
     redirect_back(fallback_location: root_path)
   end
 
   def update
     user = User.find(params[:id])
-    return current_user.confirm_friend(user) if user
+    current_user.confirm_friend(user) if user
 
     redirect_back(fallback_location: root_path)
   end
